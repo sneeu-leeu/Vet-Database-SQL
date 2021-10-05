681baaa687d7ce4ad978bd68832f6f7aeed6bfb9
sneeu-leeu <sa070197@gmail.com>
2021-10-05 16:01:04 +0200

    Add database data

--- schema.sql
+++ schema.sql
@@ -61,3 +61,8 @@ CREATE TABLE visits (
     FOREIGN KEY (vets_id) REFERENCES vets (id),
     PRIMARY KEY (id)
 );
+
+/* Audit */
+
+-- Add an email column to your owners table
+ALTER TABLE owners ADD COLUMN email VARCHAR(120);
