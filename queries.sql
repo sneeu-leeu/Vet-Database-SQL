a66be2b17afea9843fac35ed40a4f0c9cbc90fef
sneeu-leeu <sa070197@gmail.com>
2021-10-06 09:37:48 +0200

    Add index to db to speed up

--- queries.sql
+++ queries.sql
@@ -144,3 +144,7 @@ SELECT species.name, COUNT(visits.animals_id) AS species_count FROM visits
 /* Performance Audit */
 
 explain analyze SELECT COUNT(*) FROM visits where animal_id = 4;
+
+explain analyze SELECT * FROM visits where vet_id = 2;
+
+explain analyze SELECT * FROM owners where email = 'owner_18327@mail.com';
--- schema.sql
+++ schema.sql
@@ -66,3 +66,9 @@ CREATE TABLE visits (
 
 -- Add an email column to your owners table
 ALTER TABLE owners ADD COLUMN email VARCHAR(120);
+
+CREATE INDEX animal_id_asc ON visits (animal_id ASC);
+
+CREATE INDEX vet_id_asc ON visits (vet_id ASC);
+
+CREATE INDEX email_asc ON owners (email ASC);
